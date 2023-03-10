8b8136d1fd418b494dba178fdfb42701867b27b2
Martin Zeller <mz.bremerhaven@gmail.com>
2023-03-10 14:39:34 +0100

    Actual functionality

--- Plugin.cs
+++ Plugin.cs
@@ -1,36 +1,7 @@
 ﻿using System;
-using System.Reflection;
-using Assets.Scripts;
-using Assets.Scripts.Objects;
-using Assets.Scripts.Objects.Electrical;
-using Assets.Scripts.Objects.Items;
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
-
-// protected virtual void TryProcessImport()
-// {
-// 	if (ImportingThing != null)
-// 	{
-// 		for (int i = 0; i < Slots.Count; i++)
-// 		{
-// 			Slot slot = Slots[i];
-// 			if (!slot.IsInteractable)
-// 			{
-// 				if (!(slot.Occupant != null))
-// 				{
-// 					OnServer.MoveToSlot(ImportingThing, slot);
-// 					break;
-// 				}
-// 				if (i == Slots.Count - 1)
-// 				{
-// 					PlayNetworkSound(FabricatorBase.ImportErrorHash, base.InteractOnOff.Collider.transform.localPosition);
-// 				}
-// 			}
-// 		}
-// 	}
-// 	OnServer.Interact(base.InteractImport, 0);
-// }
 
 namespace StationeersStackVending
 {
@@ -39,7 +10,7 @@ namespace StationeersStackVending
     {
         public const string pluginGuid = "io.inp.stationeers.stackvending";
         public const string pluginName = "StackVending";
-        public const string pluginVersion = "0.1.0";
+        public const string pluginVersion = "1.0.0";
         private static Harmony _h;
         public static void Log(string line) {
             Debug.Log("[StackVending]: " + line);
--- VendingPatch.cs
+++ VendingPatch.cs
@@ -1,49 +1,38 @@
-
+using System.Collections.Generic;
 using Assets.Scripts.Objects;
 using Assets.Scripts.Objects.Electrical;
 using Assets.Scripts.Objects.Items;
-using Assets.Scripts.Objects.Motherboards;
 using HarmonyLib;
 
 namespace StationeersStackVending
 {
-    // [HarmonyPatch(typeof(VendingMachine), "TryProcessImport")]
-    // public class VendingPatch {
-    //     [HarmonyPrefix]
-    //     public static bool Prefix(ref VendingMachine __instance) {
-    //         Plugin.Log("TryProcessImport called");
-    //         if (__instance.ImportingThing != null) {
-    //             for (int i = 0; i < __instance.Slots.Count; i++) {
-    //                 Slot slot = __instance.Slots[i];
-    //                 if (!slot.IsInteractable) {
-    //                     if (slot.Occupant != null) {
-    //                         if (Slot.CanMerge(__instance.ImportingThing, slot)) {
-    //                             if (__instance.ImportingThing is Stackable importing) {
-    //                                 if (slot.Occupant is Stackable occupant) {
-    //                                     OnServer.Merge(occupant, importing);
-    //                                     if (occupant.Quantity <= 0) {
-    //                                         return false;
-    //                                     }
-    //                                 }
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         return true;
-    //     }
-    // }
-    //
-    [HarmonyPatch(typeof(VendingMachine))]
-    [HarmonyPatch(nameof(VendingMachine.GetLogicValue))]
+    [HarmonyPatch(typeof(VendingMachine), "TryProcessImport")]
     public class VendingPatch {
-        public static bool Prefix(ref double __result, LogicType logicType) {
-            if (logicType == LogicType.ExportCount) {
-                __result = 1337f;
-                return false;
+        [HarmonyPrefix]
+        public static bool Prefix(DynamicThing ___ImportingThing, List<Slot> ___Slots) {
+            var importingThing = ___ImportingThing;
+            if (importingThing != null) {
+                var slots = ___Slots;
+                for (int i = 0; i < slots.Count; i++) {
+                    Slot slot = slots[i];
+                    if (!slot.IsInteractable) {
+                        if (slot.Occupant != null) {
+                            if (Slot.CanMerge(importingThing, slot)) {
+                                if (importingThing is Stackable importing) {
+                                    if (slot.Occupant is Stackable occupant) {
+                                        OnServer.Merge(occupant, importing);
+                                        if (occupant.Quantity <= 0) {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             return true;
         }
     }
+
 }
